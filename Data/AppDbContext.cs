d4af5358e4d2711521b9c6d6ea55e0e94d1c1370
Demien47 <D309432@edu.curio.nl>
2026-02-04 14:20:38 +0100

    DbContext gemaakt en Connectionstring ingesteld

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WheelyGoodCars.Models;
+
+namespace WheelyGoodCars.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options)
+        : base(options) { }
+
+    public DbSet<Car> Cars { get; set; }
+    public DbSet<Tag> Tags { get; set; }
+    public DbSet<CarTag> CarTags { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<CarTag>()
+            .HasKey(ct => new { ct.CarId, ct.TagId });
+    }
+}
